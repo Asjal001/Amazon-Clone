2c789458a88def6e88999dad655f86cb525f510d
asjal <asjaldotbutt@gmail.com>
2024-09-06 23:49:57 +0500

    generating cart summary by JS

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,31 @@
-export let cart=[];
+export let cart=[
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+  }
+];
+export function addToCart(productId)
+{
+  let alreadyInCart;
+  let dropdownQuantity=document.querySelector(`.js-quantity-selector-${productId}`).value;
+  dropdownQuantity=Number(dropdownQuantity);
+  cart.forEach((cartItem)=>{
+    if(productId===cartItem.productId)
+    {
+      alreadyInCart=cartItem;
+    }
+  });
+  if(alreadyInCart){
+    alreadyInCart.quantity += dropdownQuantity;
+  }
+  else{
+    cart.push({
+      productId: productId,
+      quantity: dropdownQuantity
+    });
+  }
+}
