0f47cf4688672873b3d4283cfb74d4f55406ee06
asjal <asjaldotbutt@gmail.com>
2024-09-06 16:50:41 +0500

    13g

--- data/products.js
+++ data/products.js
@@ -29,6 +29,20 @@ const products = [
     ]
   },
   {
+    id: "d12323",
+    image: "images/products/backpack.jpg",
+    name: "Fashionable Backpack",
+    rating: {
+      stars: 4.5,
+      count: 107
+    },
+    priceCents: 1005,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },
+  {
     id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "Adults Plain Cotton T-Shirt - 2 Pack",
