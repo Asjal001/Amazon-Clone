b135e8a1d0eac937acd33fb8fffbf3c85744f42a
asjal <asjaldotbutt@gmail.com>
2024-09-12 23:27:36 +0500

    create automized test for renderOrderSummary

--- scripts/checkout/order-summary.js
+++ scripts/checkout/order-summary.js
@@ -37,7 +37,8 @@ export function renderOrderSummary()
           <div class="product-price">
             $${formatCurrency(matchingProduct.priceCents)}
           </div>
-          <div class="product-quantity">
+          <div class="product-quantity
+          js-product-quantity-${matchingProduct.id}">
             <span>
               Quantity: <span class="quantity-label quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
             </span>
@@ -46,7 +47,7 @@ export function renderOrderSummary()
             </span>
             <input type="number" class="updateInput js-update-input-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
             <span class="saveQuantityLink link-primary" data-product-id="${matchingProduct.id}">Save</span>
-            <span class="delete-quantity-link link-primary js-delete" data-product-id="${matchingProduct.id}">
+            <span class="delete-quantity-link link-primary js-delete js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
