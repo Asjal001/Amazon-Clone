8aeed3fda4ff4f8922182aa2ec7a680b4afb77ca
asjal <asjaldotbutt@gmail.com>
2024-09-16 16:51:38 +0500

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,7 @@
 import { renderOrderSummary } from "./checkout/order-summary.js";
 import { renderPaymentSummary } from "./checkout/payment-summary.js";
+import '../data/backend-practice.js';
+
+
 renderOrderSummary();
 renderPaymentSummary();
