d56b33109403fe2633c6f27c2a7cd1186d379e6b
asjal <asjaldotbutt@gmail.com>
2024-09-21 17:31:13 +0500

    learn promises and convert callback to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,17 @@ import { renderPaymentSummary } from "./checkout/payment-summary.js";
 //import '../data/backend-practice.js';
 import { loadProducts } from '../data/products.js';
 
-loadProducts(()=>{
+//creating first promise
+new Promise((resolve) => {
+  loadProducts(()=>{
+    resolve();
+  });
+}).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+// loadProducts(()=>{
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// });
