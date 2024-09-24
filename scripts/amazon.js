60462053bd7cff75c85c5aa5393b81758a40582f
asjal <asjaldotbutt@gmail.com>
2024-09-24 15:04:40 +0500

    adding search bar functionality

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -7,7 +7,20 @@ function renderProductsGrid()
   updateCartQuantity();
   //generating HTML with JS
   let HTML='';
-  products.forEach((products)=>{
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
+
+  let filteredProducts = products;
+
+  // If a search exists in the URL parameters,
+  // filter the products that match the search.
+  if (search) {
+    filteredProducts = products.filter((product) => {
+      return product.name.includes(search);
+    });
+  }
+
+    filteredProducts.forEach((products)=>{
     HTML+=`
       <div class="product-container">
         <div class="product-image-container">
@@ -82,4 +95,9 @@ function renderProductsGrid()
       }, 2000);
     });
   });
+  document.querySelector('.js-search-button')
+    .addEventListener('click', () => {
+      const search = document.querySelector('.js-search-bar').value;
+      window.location.href = `amazon.html?search=${search}`;
+    });
 }
